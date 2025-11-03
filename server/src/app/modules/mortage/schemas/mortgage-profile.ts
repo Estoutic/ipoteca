cd50ffd344f878ac12d02c00df56b4085f0a70cf
Estoutic <estoutic@gmail.com>
2025-11-03 20:57:18 +0300

    add /modules/mortage/schemas/mortgage-profile.ts
    add mortgageProfiles scheme

---
+++ server/src/app/modules/mortage/schemas/mortgage-profile.ts
@@ -0,0 +1,37 @@
+import {
+  mysqlTable,
+  varchar,
+  char,
+  timestamp,
+  decimal,
+  boolean,
+  int
+} from 'drizzle-orm/mysql-core';
+import { InferInsertModel, InferSelectModel } from 'drizzle-orm';
+
+export const mortgageProfiles = mysqlTable('MortgageProfile', {
+  id: char('id', { length: 36 }).primaryKey(),
+  userId: char('userId', { length: 36 }).notNull(),
+  propertyPrice: decimal('propertyPrice', {
+    precision: 15,
+    scale: 2
+  }).notNull(),
+  propertyType: varchar('propertyType', { length: 100 }).notNull(),
+  //TODO можно сделать реализацию через enum?
+  downPaymentAmount: decimal('downPaymentAmount', {
+    precision: 15,
+    scale: 2
+  }).notNull(),
+  matCapitalAmount: decimal('matCapitalAmount', {
+    precision: 15,
+    scale: 2
+  }),
+  matCapitalIncluded: boolean('matCapitalIncluded').notNull().default(false),
+  loanTermYears: int('loanTermYears').notNull(),
+  interestRate: decimal('interestRate', { precision: 5, scale: 2 }).notNull(),
+  createdAt: timestamp('createdAt').defaultNow().notNull(),
+  updatedAt: timestamp('updatedAt').defaultNow().onUpdateNow().notNull()
+});
+
+export type MortagePorfile = InferSelectModel<typeof mortgageProfiles>;
+export type NewMortagePorfile = InferInsertModel<typeof mortgageProfiles>;
